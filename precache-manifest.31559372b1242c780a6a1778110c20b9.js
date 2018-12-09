aa095eff4dca77a02cccc0cec338529d7d876c81
jcc2303 <cabrera.javier@gmail.com>
2018-12-09 01:33:15 +0100

    publish: with sh to deploy
    generated from commit fa00cb773f7f3faa1a429213ea3e11a0d9c26f41

---
+++ precache-manifest.31559372b1242c780a6a1778110c20b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8551b6fc42adb3b1b825",
+    "url": "/static/js/main.8551b6fc.chunk.js"
+  },
+  {
+    "revision": "6102ebb69cafd4ecfdb8",
+    "url": "/static/js/1.6102ebb6.chunk.js"
+  },
+  {
+    "revision": "8551b6fc42adb3b1b825",
+    "url": "/static/css/main.5da10a22.chunk.css"
+  },
+  {
+    "revision": "6422efc09fd755700e7aedcfcb871838",
+    "url": "/index.html"
+  }
+];
